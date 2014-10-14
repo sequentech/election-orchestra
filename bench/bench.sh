a80ad5c7135590aca975f4dec9418d9816b5a375
ruescasd <fastness@gmail.com>
2014-10-14 17:20:58 +0200

    Misc changes

--- bench/bench.sh
+++ bench/bench.sh
@@ -3,7 +3,8 @@
 # you need the unbuffer program from the expect package:
 # apt-get install expect-dev
 
-PEERS=wadobo-auth2
+# PEERS=wadobo-auth2
+PEERS=openkratio-authority
 GNU_TIME=/usr/bin/time
 
 VMN=`which vmn`
@@ -22,14 +23,37 @@ fi
 
 [ -z $VMNC ] && { echo "No vmnc"; exit 1; }
 
+if grep "/usr/bin/time" $VMN
+then
+  cp $VMN.bak $VMN
+fi
+
+if grep "/usr/bin/time" $VMNV
+then
+  cp $VMNV.bak $VMNV
+fi
+
+if grep "/usr/bin/time" $VMNC
+then
+  cp $VMNC.bak $VMNC
+fi
 
 sed -i.bak "s|^java|$GNU_TIME -v -o /tmp/vmn\${1}-$VOTES.stat java|g" $VMN
 # add verify to make it easier to parse file names in merge
 sed -i.bak "s|^java|$GNU_TIME -v -o /tmp/vmnv-verify-$VOTES.stat java|g" $VMNV
 sed -i.bak "s|^java|$GNU_TIME -v -o /tmp/vmnc\${1}-$VOTES.stat java|g" $VMNC
 
+datastore=`du -sk /home/eorchestra/election-orchestra/datastore/private/ | cut -f1`
+public=`du -sk /srv/election-orchestra/server1/public/ | cut -f1`
+before=$((public + datastore))
+
 unbuffer eotest full --peers $PEERS --vmnd --vcount $VOTES 2>&1 | tee /tmp/eotest-$VOTES.out
 
+datastore=`du -sk /home/eorchestra/election-orchestra/datastore/private/ | cut -f1`
+public=`du -sk /srv/election-orchestra/server1/public/ | cut -f1`
+after=$((public + datastore))
+
+SIZE=$((after - before))
 
 cp $VMN.bak $VMN
 cp $VMNV.bak $VMNV
@@ -41,3 +65,4 @@ cp /tmp/vmnc-plain-$VOTES.stat .
 CREATED_T=`grep "> Election created " /tmp/eotest-$VOTES.out | cut -d' ' -f4`
 TALLY_T=`grep "> Received tally data" /tmp/eotest-$VOTES.out | cut -d' ' -f5`
 echo -e "\tTotal tally time: $TALLY_T" >> ./vmn-mix-$VOTES.stat
+echo -e "\tSize on disk: $SIZE" >> ./vmn-mix-$VOTES.stat
--- bench/merge.sh
+++ bench/merge.sh
@@ -15,13 +15,17 @@ do
 
     cpu=`grep "Percent of CPU this job got" $f | cut -d' ' -f7`
     elapsed=`grep "Elapsed (wall clock) time (h:mm:ss or m:ss)" $f | cut -d' ' -f8`
-    elapsed_sec=`echo $elapsed | awk -F[:.] '{ if(NF == 3) {print ($1 * 60) + ($2 * 1) + ($3 / 100) } else {print ($1 * 3600) + ($2 * 60) + ($3 * 1) + ($4 / 100)} }'`
+    elapsed_sec=`echo $elapsed | awk -F[:.] '{ count = split($0, a, ":"); if(count == 2) {print ($1 * 60) + ($2 * 1) + ($3 / 100) } else {print ($1 * 3600) + ($2 * 60) + ($3 * 1) + ($4 / 100)} }'`
+    # elapsed_sec=`echo $elapsed | awk -F[:.] '{ if(NF == 3) {print ($1 * 60) + ($2 * 1) + ($3 / 100) } else {print ($1 * 3600) + ($2 * 60) + ($3 * 1) + ($4 / 100)} }'`
     memory=`grep "Maximum resident set size (kbytes)" $f | cut -d' ' -f6`
     tally_total=`grep "Total tally time" $f | cut -d' ' -f4`
+    size=`grep "Size on disk" $f | cut -d' ' -f4`
 
-    extra="$action $cpu $elapsed_sec $memory"
-    [ -z $tally_total ] || extra="$extra $tally_total"
-    line="${LINES[$votes]}$extra "
+    data="$action $cpu $elapsed_sec $memory"
+    [ -z $tally_total ] || data="$data $tally_total"
+    # [ -z $size ] || data="$data $size"
+    # echo data is $data
+    line="${LINES[$votes]}$data "
     LINES[$votes]="$line"
 done
 
