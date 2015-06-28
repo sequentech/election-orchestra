ee2137350d27adf89ec4d0bde955d7623a184a85
ruescasd <fastness@gmail.com>
2015-06-28 21:52:09 +0200

    Added disk.gpi, comments on clear disk

--- bench/clear_disk.sh
+++ bench/clear_disk.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# clears disk of election data, commented to avoid accidental use
+
 # rm -r -f /home/eorchestra/election-orchestra/datastore/private/*
 # rm -r -f /srv/eotest/data/*
 # rm -r -f /srv/election-orchestra/server1/public/*
