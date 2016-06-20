835a425cccc53e9027c5afc9add7b4ecdc6e2b75
Findeton <felrobelv@gmail.com>
2016-06-20 12:54:19 +0200

    update years to 2014-2016

--- bench/bench.sh
+++ bench/bench.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This file is part of election-orchestra.
-# Copyright (C) 2013  Agora Voting SL <agora@agoravoting.com>
+# Copyright (C) 2013-2016  Agora Voting SL <agora@agoravoting.com>
 
 # election-orchestra is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
