a7f85e5a7602231f8a25f8e393aafb232faf507c
Findeton <felrobelv@gmail.com>
2016-06-20 11:03:09 +0200

    add copyright headers

--- vmn.py
+++ vmn.py
@@ -1,3 +1,17 @@
+# This file is part of election-orchestra.
+# Copyright (C) 2013  Agora Voting SL <agora@agoravoting.com>
+
+# election-orchestra is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License.
+
+# election-orchestra  is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with election-orchestra.  If not, see <http://www.gnu.org/licenses/>.
 import subprocess
 from utils import *
 from frestq.app import app
