0ef18df020e43bd3c3fce72776dbd7c194da4e6a
Eduardo Robles Elvira <edulix@agoravoting.com>
2016-10-16 00:22:47 +0200

    increasing default max num of questions

--- create_election/performer_jobs.py
+++ create_election/performer_jobs.py
@@ -89,7 +89,7 @@ def check_election_data(data, check_extra):
         raise TaskError(dict(reason='no authorities'))
 
     if not isinstance(questions, list) or len(questions) < 1 or\
-            len(questions) > app.config.get('MAX_NUM_QUESTIONS_PER_ELECTION', 10):
+            len(questions) > app.config.get('MAX_NUM_QUESTIONS_PER_ELECTION', 15):
         raise TaskError(dict(reason='Unsupported number of questions in the election'))
 
 
