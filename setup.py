18b2a89e43cfe61bce54bba0f654dafba527d7b7
Eduardo Robles Elvira <edulix@gmail.com>
2021-10-14 08:57:12 +0200

    Release for version 4.0.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='election-orchestra',
-    version='4.0.0',
+    version='4.0.1',
     author='nVotes Team',
     author_email='contact@nvotes.com',
     packages=['frestq'],
@@ -17,8 +17,7 @@ setup(
     description='election orchestrator',
     long_description=open('README.md').read(),
     install_requires=[
-        'frestq @ git+https://github.com/agoravoting/frestq.git@4.0.0
-        'requests==2.22.0',
+        'frestq @ git+https://github.com/agoravoting/frestq.git@4.0.1'requests==2.22.0',
         'Flask==1.0.0',
         'Flask-SQLAlchemy==2.4.4',
         'Jinja2==2.11.3',
